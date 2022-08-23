4c70fb937db9977ff3efa9c5608012ae7f1d2ccb
YERSON2345 <yerson.bohorquez14@gmail.com>
2022-08-23 09:19:53 -0500

    migraciones

---
+++ backend/database/migrations/2022_08_23_130651_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('description', 100);
+            $table->integer('weeks');
+            $table->string('minimun_skill', 100);
+            $table->decimal('enroll_cost', $precision = 4, $scale = 2);
+            $table->foreignId('bootcamp_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+};
